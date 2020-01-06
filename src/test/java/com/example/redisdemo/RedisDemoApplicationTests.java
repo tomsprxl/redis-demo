331d8ca5ca7d523b104c4bf0ff06b668e6073b85
xuliang <18656756681@163.com>
2020-01-06 23:49:13 +0800

    add

--- src/test/java/com/example/redisdemo/RedisDemoApplicationTests.java
+++ src/test/java/com/example/redisdemo/RedisDemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.redisdemo;
 
+import com.example.redisdemo.util.RedisUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RedisDemoApplicationTests {
 
+    @Autowired
+    RedisUtil redisUtil;
+
     @Test
     void contextLoads() {
+
     }
 
 }
